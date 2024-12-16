e6ce954ef999b62a43f5a3042178298477a2bb10
codin-AWMKAR <omkaroneofakind@gmail.com>
2024-12-16 12:24:00 +0530

    EMS mk1

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+const Login = () => {
+     const[email,setEmail] = useState('');
+     const[password,setPassword]=useState('');
+   
+     //this function is used as an eventlistener to know that our form is submitted
+    const submitHandler =(e)=>{
+        e.preventDefault();
+        console.log("email is",email)
+        console.log("password is",password)
+        setEmail("")
+        setPassword("")
+    }
+ 
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+        <div className="border-2 rounded-xl border-emerald-600  p-20">
+            <form onSubmit={(e)=>{submitHandler(e)}} className="flex flex-col items-center justify-center">
+                <input required value={email} className='outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-gray-400' type="email" placeholder="Enter your email"
+                onChange={(e)=>{setEmail(e.target.value)}} />
+                <input required value={password} className='outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 px-5 text-xl mt-3 placeholder:text-gray-400' type="password" placeholder="Enter password"
+                onChange={(e)=>{setPassword(e.target.value)}}/>
+                <button  className='text-white outline-none bg-emerald-600 border-none mt-5 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-white'>Login</button>
+
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id="tasklist" className="flex overflow-x-auto flex-nowrap item-s-center justify-start gap-3 py-5 mt-10 h-[55%] w-full">
+        <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+        <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+        <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+        <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+        <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+        <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+        <div className=" flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+      
+    </div>
+  )
+}
+
+export default TaskList
